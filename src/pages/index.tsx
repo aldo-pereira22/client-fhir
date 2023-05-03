7452993c972faa3a2c66a4147d05741907b7750b
Aldo <aldopereira.gyn@gmail.com>
2023-05-03 20:07:04 -0300

    Atualização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
 
+import FHIR from 'fhirclient'
 import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react'
+
+
+const client = FHIR.client({
+  serverUrl: 'http://localhost:8080'
+})
+
 
 
 
 export default function Home() {
+  const [patient, setPatient] = useState()
+
+  // const getPatient = async () => {
+  //   var result = await client.request('Patient')
+  //   setPatient(result)
+  // }
+
+  useEffect(() => {
+
+  },[])
+
   return (
     <>
-      <h1>FHIR</h1>
+      <h1>FHIR {patient}</h1>
     </>
   )
 }
